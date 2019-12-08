4165e8fd34b83a3fa9d5608611ded5e4451e3f72
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-12-08 18:35:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -205,7 +205,7 @@ def big_shoe_rebounds
         data.each do |shoe|
 
         if shoe[:shoe] > largest_shoe
-          largest_shoe = shoe[:shoe]
+          largest_shoe = :shoe
         end
         return player_name[:rebounds]
       end
